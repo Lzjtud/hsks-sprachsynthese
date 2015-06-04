8af3eda5db767be0ced5907753ae77e7f6433595
Marccos <phenom-001@gmx.de>
2015-06-04 23:09:01 +0200

    stimmhaft gibt werte ab und bekommt weniger sinnlose werte(B)

--- stimmhaft.m
+++ stimmhaft.m
@@ -3,19 +3,21 @@
 % Erzeugung eines stimmhaften Lautes mittels einfacher Formantfilterung %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function y=stimmhaft(buchstaben,DUR,fs,B)
+function [y , f , B]=stimmhaft(buchstaben,DUR,fs,syn)
 
 %%%%%			PARAMETER			%%%%%
 if (nargin==0) buchstaben={'ae';'oe';'ue';'a';'u';'e';'i';'o'};end%Buchstaben
 if (nargin<=1) DUR=2; end %duration in sec
 if (nargin<=2) fs=44100; end %sampling freq in Hz
-if (nargin<=3) B=[100 160]; end %bandwidth
+%if (nargin<=3) B=[100 160]; end %bandwidth
+if (nargin<=3) syn=1;	end %%soll synthetisieren?
 	
 Ts=1/fs;
-B1=B(1);	%Filterbandbreite Formant 1	Acoustic Phonetics von Kenneth N. Stevens!
-B2=B(2);	%Filterbandbreite Formant 2
+%B1=B(1);	%Filterbandbreite Formant 1	Acoustic Phonetics von Kenneth N. Stevens!
+%B2=B(2);	%Filterbandbreite Formant 2
 f0=150;	% Grundschwingung, Tonhoehe
-
+f1=f2=f3=0;%damit die in FKT definiert sind...
+B1=B2=B3=0;%damit die in FKT definiert sind...
 x=sourcesignal('vokal',DUR,fs);
 disp(buchstaben);
 
@@ -97,10 +99,14 @@ for i=1:numel(buchstaben)
 			B2=177.6;
 			B3=231.4;
 	end
-	
-	y=formantfilter(x,Ts,f1,B1);	%1. Formantfilter
-	y=formantfilter(y,Ts,f2,B2);	%2. Formantfilter
-	y=formantfilter(y,Ts,f3,B3);	%2. Formantfilter
-
+	if(syn == 1)
+		y=formantfilter(x,Ts,f1,B1);	%1. Formantfilter
+		y=formantfilter(y,Ts,f2,B2);	%2. Formantfilter
+		y=formantfilter(y,Ts,f3,B3);	%2. Formantfilter
+	else 
+		y=x;
+	end
+	f=[f1 f2 f3];
+	B=[B1 B2 B3];
 	wavwrite(y'/max(y),fs,strcat('stimmhaft-',char(buchstabe),'.wav'));
 end
